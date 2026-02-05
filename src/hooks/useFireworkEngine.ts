3f25ea21f95f6afe302683e02519a7021a8004bc
HiNT <hieunt8901@gmail.com>
2026-02-05 23:54:02 +0700

    fix: correct sign in mathematical calculations for shell effects

--- src/hooks/useFireworkEngine.ts
+++ src/hooks/useFireworkEngine.ts
@@ -148,7 +148,7 @@ class Shell {
     for (let t = 0; t < PI_2; t += 0.1) {
       const x = 16 * Math.pow(Math.sin(t), 3) * scale * 0.02;
       const y =
-        (13 * Math.cos(t) -
+        -(13 * Math.cos(t) -
           5 * Math.cos(2 * t) -
           2 * Math.cos(3 * t) -
           Math.cos(4 * t)) *
@@ -167,7 +167,7 @@ class Shell {
     for (let t = 0; t < PI_2; t += 0.2) {
       const x = 16 * Math.pow(Math.sin(t), 3) * scale * 0.02;
       const y =
-        (13 * Math.cos(t) -
+        -(13 * Math.cos(t) -
           5 * Math.cos(2 * t) -
           2 * Math.cos(3 * t) -
           Math.cos(4 * t)) *
